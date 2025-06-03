1e632e3bc381bb782e85c97475046c6c8ef1b822
pelasg0 <mrdrawin983@gmail.com>
2025-06-03 12:34:06 +0200

    typed.js add

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+
 export default function Intro() {
+  const typedRef = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(typedRef.current, {
+      strings: [
+        "Backend Development.",
+        "Frontend Development.",
+        "Devops Development.",
+      ],
+      typeSpeed: 30,
+      backSpeed: 25,
+      loop: true
+    });
+    return () => typed.destroy();
+  }, []);
+
   return (
     <section className="intro">
-      <h1>INTRODUCTION</h1>
+      <h1>
+        <span ref={typedRef}></span>
+      </h1>
       <p>
         Raleway HTML5 template is provided by <a href="#">templatemo</a>. Credit goes to <a href="#">Unsplash</a> for photos.
         Feel free to modify and use this layout for any personal or commercial website.
