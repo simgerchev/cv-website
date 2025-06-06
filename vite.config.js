b9ad5be1cc3a662794604ee0a99fe578230a14e3
pelasg0 <mrdrawin983@gmail.com>
2025-06-06 11:44:33 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? process.env.VITE_BASE_PATH : '/',
+  base: command === 'build' ? '/cv-website/' : '/',
   plugins: [react()],
 }))
