bf92b0288ddab0b7bdc34bf098b37c8976a1609e
simgerchev <mrdrawin983@gmail.com>
2025-06-18 09:20:54 +0200

    path change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
         <img src={WhiteLogo} alt="Logo" className="logo-img" />
         <nav>
           <Link to="/cv-website" className="nav-link">HOME</Link>
-          <a href="#skills" className="nav-link">SKILLS</a>
-          <a href="#projects" className="nav-link">PROJECTS</a>
+          <a href="/cv-website#skills" className="nav-link">SKILLS</a>
+          <a href="/cv-website#projects" className="nav-link">PROJECTS</a>
           <Link to="/terminal" className="nav-link">TERMINAL</Link>
         </nav>
       </div>
