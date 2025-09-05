11ae4b46790c81faf919571ad393e930ee371e6f
joe <joe@joesystem.joedomain>
2025-09-05 10:55:54 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/cv-website/' : '/',
+export default defineConfig({
+  base: '/',
   plugins: [react()],
-}))
+})
